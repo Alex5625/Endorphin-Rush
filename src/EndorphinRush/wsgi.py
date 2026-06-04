b03abc549594358db332025b8a306fb910d7272b
Alex5625 <ahernandez23@alumnos.utalca.cl>
2026-06-04 15:24:11 -0400

    Commit con los requerimientos iniciales para el desarrollo grupal

---
+++ src/EndorphinRush/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for EndorphinRush project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EndorphinRush.settings')
+
+application = get_wsgi_application()
